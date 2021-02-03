918993033e77b41dfbdfe9d8587c5905be998811
lttviet <lttviet@gmail.com>
2021-02-03 14:06:17 -0500

    Make pv and cash numbers bigger

--- src/app/summary/summary-card.component.ts
+++ src/app/summary/summary-card.component.ts
@@ -4,14 +4,16 @@ import { Component, Input } from '@angular/core';
   selector: 'app-summary-card',
   template: `
     <mat-card>
-      <mat-card-title>
+      <mat-card-subtitle>
         {{ title }}
-      </mat-card-title>
-      <mat-card-content>
+      </mat-card-subtitle>
+
+      <mat-card-content class="number">
         {{ number / 100 | currency: "USD" }}
       </mat-card-content>
     </mat-card>
-  `
+  `,
+  styles: ['.number { font-size: 2em }']
 })
 export class SummaryCardComponent {
   @Input() title: string;
